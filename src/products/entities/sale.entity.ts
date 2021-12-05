2d3e314ae8edce373e4ecb88e69c566dbe506073
beku86 <luigiangelxd@gmail.com>
2021-12-04 18:10:21 -0600

    Corrección de entidad sale en la propiedad 'total'

--- src/products/entities/sale.entity.ts
+++ src/products/entities/sale.entity.ts
@@ -3,33 +3,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
 
 @ObjectType()
-@Schema()
-export class Sale {
-  @Field(() => String, {
-    description: 'Identificador único de cada venta',
-  })
-  _id: MongooseSchema.Types.ObjectId;
-
-  @Field(() => Date, {
-    description: 'Fecha y hora de la venta',
-  })
-  @Prop({ required: true })
-  date: Date;
-
-  @Field(() => [ProductSale], {
-    description: 'Productos de la venta en esa hora y fecha',
-  })
-  @Prop({ required: true, type: [] })
-  products: ProductSale[];
-
-  @Field(() => Float, {
-    description:
-      'Total de la suma de los precios de los productos y sus cantidades',
-  })
-  total: number;
-}
-
-@ObjectType()
 export class ProductSale {
   @Field(() => String, {
     description: 'Identificador único del producto vendido',
@@ -64,6 +37,34 @@ export class ProductSale {
     description: 'Total del costo por la cantidad',
   })
   totalCost: number;
+}
+
+@ObjectType()
+@Schema()
+export class Sale {
+  @Field(() => String, {
+    description: 'Identificador único de cada venta',
+  })
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Field(() => Date, {
+    description: 'Fecha y hora de la venta',
+  })
+  @Prop({ required: true })
+  date: Date;
+
+  @Field(() => [ProductSale], {
+    description: 'Productos de la venta en esa hora y fecha',
+  })
+  @Prop({ required: true, type: [] })
+  products: ProductSale[];
+
+  @Field(() => Float, {
+    description:
+      'Total de la suma de los precios de los productos y sus cantidades',
+  })
+  @Prop({ required: true })
+  total: number;
 }
 
 export type SaleDocument = Sale & Document;
